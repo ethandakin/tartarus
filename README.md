dd5472bb4b7724edfa00dad61d9898ddd2c270c9
Ethan <ethandakin@outlook.com.au>
2025-07-24 01:33:53 +1000

    wooo

---
+++ README.md
@@ -0,0 +1,3 @@
+# gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg
+Awesome
+![](https://pbs.twimg.com/media/GS9S3h1X0AAaH_D?format=jpg&name=small)
---
+++ home/home.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "ethan";
+    home.homeDirectory = "/home/ethan";
+
+    home.packages = with pkgs; [
+        fastfetch
+    ];
+
+    programs.git = {
+        enable = true;
+        userName = "ethan";
+        userEmail = "ethand@outlook.com.au";
+    };
+
+    home.stateVersion = "25.05";
+}
