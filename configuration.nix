2ad09650d3a3953b299faa7b0c86765b197bdd21
Ethan <ethandakin@outlook.com.au>
2025-07-25 00:02:20 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -41,8 +41,8 @@
     };
 
     time.timeZone = "Australia/Sydney";
-    il8n.defaultLocale = "en_US.UTF-8";
-    il8n.extraLocaleSettings = {
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
         LC_ADDRESS = "en_AU.UTF-8";
         LC_IDENTIFICATION = "en_AU.UTF-8";
         LC_MEASUREMENT = "en_AU.UTF-8";
