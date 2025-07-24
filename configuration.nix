34be4ec86a0dca3ed527f839bc71b9511ac849b0
Ethan <ethandakin@outlook.com.au>
2025-07-24 23:41:49 +1000

    ymmers

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,17 @@
 {
     imports = [
         ./hardware-configuration.nix
+        ./modules/nvidia.nix
     ];
+
+    users.users.ethan = {
+        isNormalUser = true;
+        description = "ethaana";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+            
+        ];
+    };
 
     system.stateVersion = "24.05";
 }
