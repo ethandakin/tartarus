ee8e807cbebab917217f1b1b82c3b7dc05cec2bf
Ethan <ethandakin@outlook.com.au>
2025-07-24 23:48:05 +1000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,14 +6,73 @@
         ./modules/nvidia.nix
     ];
 
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    boot.kernalPackages = pkgs.linuxPackages_latest;
+
+    networking.hostname = "nixos";
+
+    networking.networkmanager.enable = true;
+
+    programs.hyprland = {
+        enable = true;
+        withUWSM = true;
+    };
+
+    services.pulseaudio.enable = false;
+    services.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        jack.enable = true;
+
+        extraConfig.pipewire = {
+            "context.properties" = {
+                "default.clock.rate" = 48000;
+                "default.clock.quantum" = 2048;
+                "default.clock.min-quantum" = 2048;
+                "default.clock.max-quantum" = 8192;
+            };
+        };
+    };
+
+    time.timeZone = "Australia/Sydney";
+    il8n.defaultLocale = "en_US.UTF-8";
+    il8n.extraLocaleSettings = {
+        LC_ADDRESS = "en_AU.UTF-8";
+        LC_IDENTIFICATION = "en_AU.UTF-8";
+        LC_MEASUREMENT = "en_AU.UTF-8";
+        LC_MONETARY = "en_AU.UTF-8";
+        LC_NAME = "en_AU.UTF-8";
+        LC_NUMERIC = "en_AU.UTF-8";
+        LC_PAPER = "en_AU.UTF-8";
+        LC_TELEPHONE = "en_AU.UTF-8";
+        LC_TIME = "en_AU.UTF-8";
+    };
+
+    servers.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
     users.users.ethan = {
         isNormalUser = true;
         description = "ethaana";
         extraGroups = [ "networkmanager" "wheel" ];
         packages = with pkgs; [
-            
+
         ];
     };
+
+    environment.systemPackages = with pkgs; [
+        git
+    ];
 
     system.stateVersion = "24.05";
 }
